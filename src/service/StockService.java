11ddb3e67f64408fb00564e0dbb015528f4c3b8d
devyumi <yyumiwing@naver.com>
2024-08-12 10:10:05 +0900

    chore: git commit message test

---
+++ src/service/StockService.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class StockService {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public void searchStock() {
+
+    }
+
+    public void searchStockByLargeCategory() {
+
+    }
+
+    public void searchStockByMidCategory() {
+
+    }
+
+    public void searchStockBySmallCategory() {
+
+    }
+
+    public void searchStockByWarehouse() {
+
+    }
+
+    public void searchStockByVendor() {
+
+    }
+}
